d171dc7608dd8dc0f416cab222e62bc8b880c30d
Boondock19 <37387864+Boondock19@users.noreply.github.com>
2020-01-02 21:23:37 -0400

    2

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -81,10 +81,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
 // *** start ***
 // On first load, show home view
 showLoading("#main-content");
-$ajaxUtils.sendGetRequest(
-  allCategoriesUrl,
-  buildAndShowHomeHTML, // ***** <---- TODO: STEP 1: Substitute [...] ******
-  true); // Explicitely setting the flag to get JSON from server processed into an object literal
+$ajaxUtils.sendGetRequest(allCategoriesUrl,buildAndShowHomeHTML,true); 
 });
 // *** finish **
 
